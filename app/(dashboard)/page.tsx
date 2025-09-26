3a8cf1e79508aa606fa386c9e07120422dbaa84b
jihanv <hedjmny@gmail.com>
2025-09-26 19:14:59 +0900

    25:00 Add Clerk for authentication

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,5 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
   return (
-    <div>
-      <Button>Click Me</Button>
-    </div>
+    <p>This is an authenticated route.</p>
   );
 }
