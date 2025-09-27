3101843de8575ab688f13e9202cf1f935ae5dede
jihanv <hedjmny@gmail.com>
2025-09-27 21:29:23 +0900

    1:13:00 Add layout for dashboard route.

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
-import { z } from "zod";
-import { zValidator } from "@hono/zod-validator";
+// import { z } from "zod";
+// import { zValidator } from "@hono/zod-validator";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 
 const app = new Hono().basePath("/api");
@@ -9,6 +9,7 @@ const app = new Hono().basePath("/api");
 app.get("/hello", clerkMiddleware(), (c) => {
   const auth = getAuth(c);
 
+  // Check if user is authorized
   if (!auth?.userId) {
     return c.json({
       error: "Unauthorized",
