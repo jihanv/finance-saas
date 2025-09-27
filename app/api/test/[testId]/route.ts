d25b67f86673808c7d865b4714b5826244caa14d
jihanv <hedjmny@gmail.com>
2025-09-27 20:05:03 +0900

    47:00 Add hono package

--- app/api/test/[testId]/route.ts
+++ app/api/test/[testId]/route.ts
@@ -1,6 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function GET(request: NextRequest, { params }: { testId: string }) {
+export function GET(
+  request: NextRequest,
+  { params }: { params: { testId: string } }
+) {
   return NextResponse.json({
     hello: "good",
     testId: params.testId,
