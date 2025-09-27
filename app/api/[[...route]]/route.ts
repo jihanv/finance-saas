f026a6435fd433ccbd355bdc67e275872f1ac073
jihanv <hedjmny@gmail.com>
2025-09-27 20:24:57 +0900

    56:00 Add zod package to use hono zod validator

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,13 +1,23 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
 
 const app = new Hono().basePath("/api");
 
-app.get("/hello", (c) => {
-  return c.json({
-    message: "Hello Next.js!",
+app
+  .get("/hello", (c) => {
+    return c.json({
+      message: "Hello Next.js!",
+    });
+  })
+  .get("hello/:test", (c) => {
+    const test = c.req.param("test");
+    return c.json({
+      message: "Hello World",
+      test: test,
+    });
   });
-});
 
 export const GET = handle(app);
 export const POST = handle(app);
