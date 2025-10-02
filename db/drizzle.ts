22e6c36a78877dd050b47910973a4a9078044a48
jihanv <hedjmny@gmail.com>
2025-10-02 20:26:37 +0900

    1:56:00 Add account schema

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,7 +1,13 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 
-const sql = neon(process.env.DATABASE_URL!);
-const db = drizzle({ client: sql });
+// Sets up a database connection using Neon (serverless Postgres)
+// and wraps it with Drizzle ORM for type-safe queries.
+
+// - `sql`: low-level Neon client for running raw SQL
+export const sql = neon(process.env.DATABASE_URL!);
+
+// - `db`: Drizzle ORM instance for structured, type-safe database access
+export const db = drizzle({ client: sql });
 
 // const result = await db.execute("select 1");
