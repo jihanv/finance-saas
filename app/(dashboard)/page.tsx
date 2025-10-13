68d804b229ab718bf9c2a9f2ee13f80c29180a58
jihanv <hedjmny@gmail.com>
2025-10-13 11:57:49 +0900

    Fetch and display accounts in dashboard page.tsx

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,10 +1,22 @@
 // import { UserButton } from "@clerk/nextjs"
+"use client"
+
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
 
 export default function Home() {
+
+  const accountsQuery = useGetAccounts();
+
+  if (accountsQuery.isLoading) {
+    return (<div>Loading...</div>)
+  }
   return (
     <>
-      {/* <UserButton /> */}
-      <h1>Dashboard Page</h1>
+      <div>
+        {accountsQuery.data?.map((account) => (
+          <div key={account.id}>{account.name}</div>
+        ))}
+      </div>
     </>
 
   );
