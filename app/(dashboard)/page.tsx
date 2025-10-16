3daca99ea9a8c882ed411f2bec4aff3485fb6235
jihanv <hedjmny@gmail.com>
2025-10-16 18:39:35 +0900

    3:03:00 Allow sheet to be open and closed

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,14 +1,19 @@
 // import { UserButton } from "@clerk/nextjs"
 "use client"
 
+import { Button } from "@/components/ui/button";
+import { useNewAccount } from "@/stores/useNewAccountStore";
+
 // import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
 
 export default function Home() {
 
+  const { onOpen } = useNewAccount()
+
   return (
     <>
       <div>
-        Home Page
+        <Button onClick={onOpen}>Add an Account</Button>
       </div>
     </>
 
--- features/accounts/components/new-account-sheet.tsx
+++ features/accounts/components/new-account-sheet.tsx
@@ -5,12 +5,15 @@ import {
     SheetHeader,
     SheetTitle
 } from '@/components/ui/sheet'
+import { useNewAccount } from '@/stores/useNewAccountStore'
 import React from 'react'
 
 export default function NewAccountSheet() {
+
+    const { isOpen, onOpen, onClose } = useNewAccount()
     return (
         <>
-            <Sheet open>
+            <Sheet open={isOpen} onOpenChange={onClose}>
                 <SheetContent className='space-y-4'>
                     <SheetHeader>
                         <SheetTitle>
