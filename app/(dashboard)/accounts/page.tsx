35895326a77fbee25dae65115706f38fd4acb9ee
jihanv <hedjmny@gmail.com>
2025-10-29 20:36:48 +0900

    3:40:00 Render Data Table with dummy data for testing.

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -1,12 +1,23 @@
 "use client"
 
 import { Button } from '@/components/ui/button'
-import { Card, CardHeader, CardTitle } from '@/components/ui/card'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { useNewAccount } from '@/stores/useNewAccountStore'
 import { Plus } from 'lucide-react'
 import React from 'react'
+import { columns, Payment } from './columns'
+import { DataTable } from '@/components/data-table'
 
 export default function Accounts() {
+
+    const data: Payment[] = [
+        {
+            id: "728ed52f",
+            amount: 100,
+            status: "pending",
+            email: "m@example.com",
+        },
+    ]
 
     const newAccounts = useNewAccount()
     return (
@@ -18,6 +29,9 @@ export default function Accounts() {
                         <Plus className='size-4 mr-2' ></Plus>
                         Add new</Button>
                 </CardHeader>
+                <CardContent>
+                    <DataTable columns={columns} data={data} />
+                </CardContent>
             </Card>
         </div>
     )
