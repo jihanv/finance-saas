04cb80b5f0c8c2b10003354044c2e7d4c70523ae
jihanv <hedjmny@gmail.com>
2025-11-03 18:45:09 +0900

    4:24:00 Implement a use-confirm hook.

---
+++ hooks/use-confirm.tsx
@@ -0,0 +1,57 @@
+import { Button } from '@/components/ui/button'
+import { Dialog, DialogContent, DialogDescription, DialogTitle, DialogFooter, DialogHeader } from '@/components/ui/dialog'
+import React, { JSX, useState } from 'react'
+
+export type UseConfirmProps = {
+    title: string,
+    message: string
+}
+
+export default function useConfirm({ title, message }: UseConfirmProps)
+    : [() => JSX.Element, () => Promise<unknown>] {
+
+    const [promise, setPromise] = useState<{ resolve: (value: boolean) => void } | null>(null)
+
+    const confirm = () => new Promise((resolve, reject) => {
+        setPromise({ resolve })
+    })
+    const handleClose = () => {
+        setPromise(null);
+    };
+
+    const handleConfirm = () => {
+        promise?.resolve(true);
+        handleClose();
+    };
+
+    const handleCancel = () => {
+        promise?.resolve(false);
+        handleClose();
+    };
+
+    const ConfirmationDialog = () => (
+        <Dialog open={promise !== null}>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>{title}</DialogTitle>
+                    <DialogDescription>{message}</DialogDescription>
+                </DialogHeader>
+                <DialogFooter className='pt-2'>
+                    <Button
+                        onClick={handleCancel}
+                        variant="outline">
+                        Cancel
+                    </Button>
+                    <Button
+                        onClick={handleConfirm}
+                    >
+                        Confirm
+                    </Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    )
+
+
+    return [ConfirmationDialog, confirm]
+}
