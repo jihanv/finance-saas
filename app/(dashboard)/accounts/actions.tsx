f30a59ff5f0fa2172da104c525191b32d9731f52
jihanv <hedjmny@gmail.com>
2025-11-03 20:23:07 +0900

    4:41:00 implement actions and add dropdown menu for editing.

--- app/(dashboard)/accounts/actions.tsx
+++ app/(dashboard)/accounts/actions.tsx
@@ -1,10 +1,30 @@
 "use client"
 
+import { Button } from "@/components/ui/button"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import { Edit, MoreHorizontal } from "lucide-react"
+
 type Props = {
     id: string,
 }
 export default function Actions({ id }: Props) {
     return (
-        <div>Actions</div>
+        <>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="ghost" className="size-8 p-0">
+                        <MoreHorizontal />
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                    <DropdownMenuItem
+                        disabled={false}
+                        onClick={() => { }}>
+                        <Edit className="size-4 mr-2" />
+                        Edit
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </>
     )
 }
