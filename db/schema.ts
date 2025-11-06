68631c55dd9ad086fef65957fc0ebb12bbbc0c6a
jihanv <hedjmny@gmail.com>
2025-11-06 23:29:41 +0900

    5:04:00 Add categories schema.

--- db/schema.ts
+++ db/schema.ts
@@ -12,3 +12,10 @@ export const insertAccountSchema = createInsertSchema(accounts);
 //2:09:00 Add a new column (plaidId)
 // pnpm run db:generate
 // pnpm run db:migrate
+
+export const categories = pgTable("categories", {
+  id: text("id").primaryKey(),
+  plaidId: text("plaid_id"),
+  name: text("name").notNull(),
+  userId: text("user_id").notNull(),
+});
