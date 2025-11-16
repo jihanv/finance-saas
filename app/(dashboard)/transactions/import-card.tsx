a6b1cf33d803c79d533d7c9ba8bbd338aac2ed76
jihanv <hedjmny@gmail.com>
2025-11-16 14:19:35 +0900

    8:40 Add begin implemenation of handleContinue. Right now, it just formats the date.

--- app/(dashboard)/transactions/import-card.tsx
+++ app/(dashboard)/transactions/import-card.tsx
@@ -7,6 +7,7 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
 import ImportTable from "./import-table";
 
+// This object can have any number of properties, and each property name must be a string. It needs to be dynamic because you don’t know how many CSV columns there are.
 interface SelectedColumnsState {
     [key: string]: string | null
 }
@@ -19,14 +20,18 @@ type Props = {
 
 export default function ImportCard({ data, onCancel, onSubmit }: Props) {
 
+    // selectedColumns is an object that keeps track of which CSV column the user mapped to which field. The value inside this state will match the SelectedColumnsState interface.
     const [selectedColumns, setSelectedColumns] = useState<SelectedColumnsState>({})
 
+    // data is the parsed CSV file.
     const headers = data[0]
     const body = data.slice(1)
 
     const onTableHeadSelectChange = (columnIndex: number, value: string | null) => {
         setSelectedColumns((prev) => {
             const newSelectedColumns = { ...prev }
+
+            // Defensive check. If any other column already has this value, clear it.
             for (const key in newSelectedColumns) {
                 if (newSelectedColumns[key] === value) {
                     newSelectedColumns[key] = null
@@ -36,12 +41,51 @@ export default function ImportCard({ data, onCancel, onSubmit }: Props) {
                 value = null
             }
 
+            // Assign the new value to the chosen column
             newSelectedColumns[`column_${columnIndex}`] = value
             return newSelectedColumns
         })
     }
-
+    // returns: The number of columns the user has successfully mapped.
+    // Object.values(selectedColumns) ->This takes all the values of the object and puts them in an array. It does NOT keep the keys — only the values.
+    // .filter(Boolean) -> .filter(Boolean) removes any “falsy” values (null, undefined,etc). This gives only the columns that have been assigned a value.
+    //.length -> Now we count how many selected columns there are.
     const progress = Object.values(selectedColumns).filter(Boolean).length
+
+    const handleContinue = () => {
+        const getColumnIndex = (column: string) => {
+            return column.split("_")[1]
+        }
+        const mappedData = {
+            headers: headers.map((_header, index) => {
+                const columnIndex = getColumnIndex(`column_${index}`)
+                return selectedColumns[`column_${columnIndex}`] || null
+            }),
+            body: body.map((row) => {
+                const transformedRow = row.map((cell, index) => {
+                    const columnIndex = getColumnIndex(`column_${index}`);
+                    return selectedColumns[`column_${columnIndex}`] ? cell : null
+                });
+
+                return transformedRow.every((item) => item === null)
+                    ? []
+                    : transformedRow;
+            }).filter((row) => row.length > 0)
+        }
+
+        const arrayofData = mappedData.body.map((row) => {
+            return row.reduce((acc: any, cell, index) => {
+                const header = mappedData.headers[index];
+                if (header !== null) {
+                    if (acc !== null) {
+                        acc[header] = cell
+                    }
+                }
+                return acc;
+            }, {})
+        })
+        console.log({ arrayofData })
+    }
 
     return (
         <>
@@ -58,7 +102,7 @@ export default function ImportCard({ data, onCancel, onSubmit }: Props) {
                             </Button>
                             <Button
                                 disabled={progress < requiredOptions.length}
-                                onClick={() => { }}
+                                onClick={handleContinue}
                                 size="sm"
                                 className="w-full lg:w-auto"
                             >
--- app/(dashboard)/transactions/table-head-select.tsx
+++ app/(dashboard)/transactions/table-head-select.tsx
@@ -29,12 +29,15 @@ export default function TableHeadSelect({ columnIndex, selectedColumns, onChange
                     value={currentSelection || ""}
                     onValueChange={(value) => onChange(columnIndex, value)}
                 >
+                    {/* the part of the Select component that you actually see BEFORE opening the menu. */}
                     <SelectTrigger className={
                         cn("focus:ring-offset-0 focus:ring-transparent outline-none border-none bg-transparent capitalize", currentSelection && "text-blue-500")
                     } >
+                        {/* Displays the current selection inside the trigger */}
                         <SelectValue placeholder="Skip"></SelectValue>
                     </SelectTrigger>
                     <SelectContent>
+                        {/* Skip is always included in the dropdown, it never gets disabled */}
                         <SelectItem value="skip">Skip</SelectItem>
                         {options.map((option, index) => {
                             const disabled = Object.values(selectedColumns).includes(option) && selectedColumns[`column_${columnIndex}`] !== option
