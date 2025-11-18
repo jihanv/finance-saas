2361a602472b0933796fa3776a3495763113688e
jihanv <hedjmny@gmail.com>
2025-11-18 11:46:44 +0900

    Add formatDateRange util function.

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,42 +1,17 @@
 // import { UserButton } from "@clerk/nextjs"
-"use client"
 
-import { Button } from "@/components/ui/button";
-import { useNewAccount } from "@/stores/useNewAccountStore";
-
+import DataGrid from "@/components/data-grid";
 
 // import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
 
-export default function Home() {
-
-  const { onOpen } = useNewAccount()
+export default function DashboardPage() {
 
   return (
     <>
-      <div>
-        <Button onClick={onOpen}>Add an Account</Button>
+      <div className="max-w-screen 2xl mx-auto w-full pb-10 -mt-24">
+        <DataGrid />
       </div>
     </>
 
   );
 }
-
-
-// export default function Home() {
-
-//   const accountsQuery = useGetAccounts();
-
-//   if (accountsQuery.isLoading) {
-//     return (<div>Loading...</div>)
-//   }
-//   return (
-//     <>
-//       <div>
-//         {accountsQuery.data?.map((account) => (
-//           <div key={account.id}>{account.name}</div>
-//         ))}
-//       </div>
-//     </>
-
-//   );
-// }
